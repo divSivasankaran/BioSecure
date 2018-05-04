0bd1494da2a3c7463809ea4d8a6957170e449503
Divya Sivasankaran <e0013178@u.nus.edu>
2018-05-04 15:56:01 +0800

    bug fix
    API changed from put to post

--- Backend/project/tests/enroll.py
+++ Backend/project/tests/enroll.py
@@ -2,7 +2,7 @@ import requests
 import os
 import json
 
-with open(os.path.join('test_2.jpg'), 'rb') as fp:
+with open(os.path.join('obama1.jpg'), 'rb') as fp:
     # file = FileStorage(fp)
-    response = requests.put('http://localhost:5000/biosecure/api/v1/enroll', files={'image': fp})
+    response = requests.post('http://localhost:5000/biosecure/api/v1/enroll', files={'image': fp})
     print(response)
--- Backend/project/tests/verify.py
+++ Backend/project/tests/verify.py
@@ -2,6 +2,6 @@ import requests
 import os
 import json
 
-with open(os.path.join('test_3.jpg'), 'rb') as fp:
-    response = requests.put('http://localhost:5000/biosecure/api/v1/verify', files={'image': fp})
+with open(os.path.join('obama1.jpg'), 'rb') as fp:
+    response = requests.post('http://localhost:5000/biosecure/api/v1/verify', files={'image': fp})
     print(response.text)
